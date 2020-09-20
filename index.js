66e65be08907014a663f67ad92a1439fe1417abb
Mike Stop Continues <mikestopcontinues@gmail.com>
2020-09-20 17:37:23 -0500

    Port from monorepo

---
+++ index.js
@@ -0,0 +1,32 @@
+// vars
+
+const env =
+  process.env.BROWSERSLIST_ENV ||
+  process.env.NODE_ENV ||
+  'development';
+
+// vars
+
+const config = {
+  development: [
+    'last 1 version and last 1 year',
+    'unreleased versions',
+    'node >= 14',
+    'not dead',
+    'not android > 0',
+  ],
+
+  production: [
+    'last 2 years',
+    'unreleased versions',
+    'node >= 12',
+    '> 0.2%',
+    'not dead',
+    'not ie > 0',
+    'not android > 0',
+  ],
+};
+
+// export
+
+module.exports = config[env];
